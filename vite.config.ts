3d6211989d7d9378489c3eea89bddd5eb0420177
sencin <asunayuk34@gmail.com>
2026-04-13 21:40:17 +0800

    removed vendor splititng

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  build: {
-    chunkSizeWarningLimit: 1000, 
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        },
-      },
-    },
-  },
 })
